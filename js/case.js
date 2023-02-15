49e4a3b3d256e816a9ea36ddbf28293de0bf8c7c
Rihanul Islam <rihanthedev@gmail.com>
2023-02-15 16:39:45 +0600

    CaseMinus

--- js/case.js
+++ js/case.js
@@ -5,6 +5,11 @@ document.getElementById('btn-case-plus').addEventListener('click', function(){
     const previousCaseNumber = parseInt(caseNumberString);
     const newCaseNumber = previousCaseNumber+ 1;
     caseNumberFiled.value = newCaseNumber;
+    const totalCaseElement = document.getElementById('total-case-amount');
+    const totalCaseString = totalCaseElement.innerText;
+    const CasePrice = 59;
+    const totalCaseAmount = CasePrice*newCaseNumber;
+    totalCaseElement.innerText = totalCaseAmount;
 })
 // btn-case-minus
 document.getElementById('btn-case-minus').addEventListener('click', function(){
@@ -12,6 +17,13 @@ document.getElementById('btn-case-minus').addEventListener('click', function(){
     const caseNumberMinusString = caseNumberMinusFiled.value;
     const previousCaseMinusNumber = parseInt(caseNumberMinusString);
     const newCaseMinus = previousCaseMinusNumber-1;
-    caseNumberMinusFiled.value = newCaseMinus;
-    
+    if(newCaseMinus >=0){
+        caseNumberMinusFiled.value = newCaseMinus; 
+        const totalCaseElementMinus = document.getElementById('total-case-amount');
+        const totalCaseMinusString = totalCaseElementMinus.innerText;
+        const totalCaseMinus =parseInt(totalCaseElementMinus)
+        const totalMinusPrice =newCaseMinus*59;
+        // console.log(previousCaseMinusNumber);
+        totalCaseElementMinus.innerText = totalMinusPrice;
+    } 
 })
